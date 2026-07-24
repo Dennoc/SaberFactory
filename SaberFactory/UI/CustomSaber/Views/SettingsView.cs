6b4328711dae5b74ee9a3802c094d50136834ef1
Dennoo11 <deancrecca1@gmail.com>
2026-07-24 20:55:12 +1000

    Update SettingsView.cs

--- SaberFactory/UI/CustomSaber/Views/SettingsView.cs
+++ SaberFactory/UI/CustomSaber/Views/SettingsView.cs
@@ -16,7 +16,7 @@ namespace SaberFactory.UI.CustomSaber.Views
     {
         private const string ProfileUrl = "https://github.com/ToniMacaroni";
         private const string DiscordUrl = "https://github.com/ToniMacaroni/SaberFactory";
-        private const string ProfileTwoUrl = "https://github.com/Dennoo11";
+        private const string ProfileTwoUrl = "https://github.com/Dennoc";
 
         [UIComponent("changelog-popup")] private readonly ChangelogPopup _changelogPopup = null;
         [UIObject("github-button")] private readonly GameObject _githubButton = null;
