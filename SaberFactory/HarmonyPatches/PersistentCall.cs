39189c138a40e5b814763cb1f8110b3c8f448688
Dennoo11 <deancrecca1@gmail.com>
2026-08-25 14:33:46 +1000

    Create PersistentCall.cs

---
+++ SaberFactory/HarmonyPatches/PersistentCall.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+
+namespace SaberFactory.HarmonyPatches
+{
+    /// <summary>
+    /// This patch removes any serialized m_TargetAssemblyTypeName since that field can be used for bad things, potentially including arbitrary code execution.
+    /// See https://blog.includesecurity.com/2021/06/hacking-unity-games-malicious-unity-game-objects/ for details.
+    /// </summary>
+    [HarmonyPatch]
+    internal class PersistentCall_OnAfterDeserialize
+    {
+        private static readonly Type PersistentCallType = Type.GetType("UnityEngine.Events.PersistentCall, UnityEngine.CoreModule", true);
+        private static readonly MethodInfo OnAfterDeserializeMethod = AccessTools.DeclaredMethod(PersistentCallType, "OnAfterDeserialize");
+        private static readonly FieldInfo TargetAssemblyTypeName = AccessTools.DeclaredField(PersistentCallType, "m_TargetAssemblyTypeName");
+
+        [HarmonyPriority(Priority.Last)]
+        public static void Postfix(object __instance)
+        {
+            TargetAssemblyTypeName.SetValue(__instance, null);
+        }
+
+        public static MethodBase TargetMethod() => OnAfterDeserializeMethod;
+    }
+}
